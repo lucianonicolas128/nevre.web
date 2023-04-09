76061b68b22f3e7c20828be2a46cfc5f32c3e771
Luciano Nicolas Prina <luciano.prina@hotmail.es>
2023-04-09 14:20:15 -0300

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    apiUrl: process.env.REACT_APP_API_HOST || 'http://localhost:3001',
+    subKey: process.env.API_KEY_SUBSCRIPTION || '',
+  },
+  compress: true,
+  // TODO
+  // basePath: '/',
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
