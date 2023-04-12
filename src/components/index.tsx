b942d029f4f66c698f81cd59875511d1a2ac0bb2
Luciano Nicolas Prina <luciano.prina@hotmail.es>
2023-04-12 12:38:31 -0300

    Head

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -18,6 +18,11 @@ const HeadApp = (props: any) => {
         content="Desarrollo de software, desarrollo de apps, desarrollo de aplicaciones mobile y web. Programación. Empresa de programación. Sistemas de gestión de negocios y emprendimientos"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="keywords"
+        content="aplicaciones, apps, development, desarrollo, software, software de gestión, software de gestion, aplicaicones web, aplicacion web, desarrollo de aplicaciones web, desarrollo de aplicaciones para telefonos, desarrollo de apps, gestion, gestión, gestion de negocios, gestion de emprendimientos, software de gestión de negocios, software de gestión de emprendimientos, apps para gestion de negocios, apps apra gestión de negocios, apps para gestión de emprendimiento"
+      />
+
       <link
         rel="icon"
         href="https://res.cloudinary.com/lucianonicolas/image/upload/v1681166409/levelapp-logo-circle_iyy2x6.png"
